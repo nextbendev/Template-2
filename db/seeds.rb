92f0355f25acb003801206df5898d23e4cf2ebc7
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-21 15:05:38 -0400

    template 2

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# Create a main sample user.
+User.create!(name:  "Gastro Development",
+             email: "admin@example.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+# Generate a bunch of additional users.
+20.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
+
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+10.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Create following relationships.
+users = User.all
+user  = users.first
+following = users[2..20]
+followers = users[3..10]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
